3bd91ca5242e860e8d9a1ecb7d3a212503f2dbf4
Peter Na <ruikangna0605@hotmail.com>
2022-12-04 20:58:19 -0800

    add response models

---
+++ src/components/finder/Finder.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Find.css'
+import { Link } from "react-router-dom";
+
+const Find = () => {
+  const findPlace = () => {
+
+  }
+  
+  return (
+    <Link to="/placedetails">
+      <button 
+        className='find'
+        onClick={findPlace}>
+      Find
+      </button>
+    </Link>
+  )
+}
+
+export default Find
