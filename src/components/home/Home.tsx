a22e8d05771a3416bad08c280c401d3e6c2360cf
Peter Na <ruikangna0605@hotmail.com>
2023-03-23 23:02:27 -0700

    initialize footer and home component

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../footer/Footer'
+import Navigation from '../navigation/Navigation'
+
+const Home = () => {
+  return (
+    <div>
+        <Navigation />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
