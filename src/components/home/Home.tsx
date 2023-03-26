21e185e440fec0f3353e797d761e505707abe1fa
Peter Na <ruikangna0605@hotmail.com>
2023-03-25 22:37:16 -0700

    add background image to home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Footer from '../footer/Footer'
 import Navigation from '../navigation/Navigation'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
         <Navigation />
+        <div className='page-hero'>
+            <div className='container'>
+                <h1>Findine</h1>
+                <p>Connect your ideas globally</p>
+                <a className="btn" href="#">Learn More</a>
+            </div>
+        </div>
         <Footer />
     </div>
   )
