c4125dba0d5c83b658acf2866ce00671c187fd2f
Peter Na <ruikangna0605@hotmail.com>
2023-04-08 01:33:57 -0700

    update navigation bar

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import './Navigation.css'
+import "./Navigation.css";
 
 const Navigation = () => {
   return (
-    <div className='navigation'>
-      <h1 className='title'>Findine</h1>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#find">Find</a></li>
-        <li><a href="#search">Search</a></li>
-        <li><a href="#browse">Browse</a></li>
-      </ul>
-    </div>
-  )
-}
+    <header>
+      <nav>
+        <div className="logo">
+          <h1>
+            <a href="/">Findine</a>
+          </h1>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/find">Find</a>
+          </li>
+          <li>
+            <a href="/browse">Browse</a>
+          </li>
+          <li>
+            <a href="/search">Search</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Navigation
+export default Navigation;
