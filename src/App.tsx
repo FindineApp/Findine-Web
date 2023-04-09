6e5c4a9ae5c08e368ed8ced5c91bde00a517ce6b
Peter Na <ruikangna0605@hotmail.com>
2023-04-09 11:16:32 -0700

    update app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import './App.css';
-import Navigation from './components/navigation/Navigation';
+import Header from './components/header/Header';
 import Finder from './components/finder/Finder';
 import { Map } from './components/map/Map';
-import { SearchBar } from './components/search-bar/SearchBar';
+import { Search } from './components/search/Search';
 import Places from './components/places/Places';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
+import Browse from './components/browse/Browse';
 
 function App() {
   return (
     <main className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Map />} />
-        <Route path="/pets" element={<SearchBar />} />
-        <Route path="/addPets" element={<Places />} />
+        <Route path="/find" element={<Finder />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/search" element={<Search />} />
       </Routes> 
     </main>
   );
